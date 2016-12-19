12b4166469f228b569b3e46e155208d212bde357
Lewis Kabui <lewis.maina@andela.com>
2016-12-19 18:06:34 +0300

    Create ListTest class

---
+++ ListTest.java
@@ -0,0 +1,46 @@
+import com.deitel.jhtp4.ch19.List;
+import com.deitel.jhtp4.ch19.EmptyListException;
+
+public class ListTest {
+	public static void main(String args[]) {
+		List list = new List();
+
+		Boolean bool = Boolean.TRUE;
+		Character character = new Character('$');
+		Integer integer = new Integer(34567);
+		String string = "hello";
+
+		list.insertAtFront(bool);
+		list.print();
+		list.insertAtFront(character);
+		list.print();
+		list.insertAtFront(integer);
+		list.print();
+		list.insertAtFront(string);
+		list.print();
+
+		Object removedObject;
+
+		try {
+			removedObject = list.removeFromFront();
+			System.out.println(removedObject.toString() + " removed");
+			list.print();
+
+			removedObject = list.removeFromFront();
+			System.out.println(removedObject.toString() + " removed");
+			list.print();
+
+			removedObject = list.removeFromBack();
+			System.out.println(removedObject.toString() + " removed");
+			list.print();
+			
+
+			removedObject = list.removeFromBack();
+			System.out.println(removedObject.toString() + " removed");
+			list.print();
+		}
+		catch (EmptyListException emptyListException) {
+			emptyListException.printStackTrace();
+		}
+	}
+}
