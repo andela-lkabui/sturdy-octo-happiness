38045d9422648770be1275d9ad25067868e7e7e3
Lewis Kabui <lewis.maina@andela.com>
2016-12-20 17:08:41 +0300

    Create a test for the Tree

---
+++ TreeTest.java
@@ -0,0 +1,26 @@
+import com.deitel.jhtp4.ch19.Tree;
+
+public class TreeTest {
+	
+	public static void main(String args[]) {
+		Tree tree = new Tree();
+		int value;
+
+		System.out.println("Inserting the following values: ");
+
+		for (int i = 1; i <= 10; i++) {
+			value = (int) (Math.random() * 100);
+			System.out.println(value + " ");
+
+			tree.insertNode(value);
+		}
+
+		System.out.println("\n\nPreorder traversal");
+		tree.preorderTraversal();
+
+		System.out.println("\n\nInorder traversal");
+		tree.inorderTraversal();
+		System.out.println("\n\nPostorder traversal");
+		tree.postorderTraversal();
+	}
+}
